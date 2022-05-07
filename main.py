2e1582766df10e4dabe8af1045bae03f8819ce8d
omrastgi <omrastogi@gmail.com>
2022-05-08 01:49:38 +0530

    Added req, made the ner code modular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # from ner1 import get_entity
-from ner2 import get_entity
+from ner2 import RobertaNER
 from read_data import get_data
 
 
@@ -7,39 +7,40 @@ def get_paras():
     return get_data()
 
 
-def get_names(num):
+def get_names(model, num):
     try:
         text = get_data()[num]
     except Exception as e:
-        print(e)
+        return e
     names = []
     try:
-        ners = get_entity(text)
+        ners = model.get_entity(text=text)
         for ner in ners:
             if ner[0] == 'PER':
                 names.append(ner[1])
     except Exception as e:
-        print(e)
-    return names
+        return e
+    return set(names)
 
 
-def get_places(num):
+def get_places(model, num):
     try:
         text = get_data()[num]
     except Exception as e:
-        print(e)
+        return e
     places = []
     try:
-        ners = get_entity(text)
+        ners = model.get_entity(text=text)
         for ner in ners:
             if ner[0] == 'LOC':
                 places.append(ner[1])
     except Exception as e:
-        print(e)
-    return places
+        return e
+    return set(places)
 
 
 if __name__ == '__main__':
     i = 1
-    print(get_places(i))
-    print(get_names(i))
+    model = RobertaNER()
+    print(get_places(model, i))
+    print(get_names(model, i))
--- ner2.py
+++ ner2.py
@@ -1,46 +1,32 @@
 import torch
-from transformers import RobertaTokenizer, RobertaForTokenClassification
 from transformers import pipeline
-
-tokenizer = RobertaTokenizer.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
-model = RobertaForTokenClassification.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
+from transformers import RobertaTokenizer, RobertaForTokenClassification
 
 
-def get_entity(text):
-    nlp = pipeline("ner", model=model, tokenizer=tokenizer)
-    entities = nlp(text)
-    words = []
-    labels = []
-    for i, ety in enumerate(entities):
-        if ord(ety['word'][0]) == 288:
-            words.append(ety['word'])
-            labels.append(ety['entity'])
-        else:
-            words[-1] += ety['word']
+class RobertaNER:
+    def __init__(self):
+        self.tokenizer = RobertaTokenizer.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
+        self.model = RobertaForTokenClassification.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
 
-    output = []
-    for word, label in zip(words, labels):
-        output += [[label, word[1:]]]
+    def get_entity(self, text):
+        nlp = pipeline("ner", model=self.model, tokenizer=self.tokenizer)
+        entities = nlp(text)
+        words = []
+        labels = []
+        for i, ety in enumerate(entities):
+            if ord(ety['word'][0]) == 288:
+                words.append(ety['word'])
+                labels.append(ety['entity'])
+            else:
+                words[-1] += ety['word']
 
-    return output
+        output = []
+        for word, label in zip(words, labels):
+            output += [[label, word[1:]]]
+
+        return output
 
 
 if __name__ == '__main__':
-    from read_data import get_data
-    text = get_data()[1][0:100]
-    # text = "The new assembly hall, Dasaratha’s latest pride, was crowded all"
-    inputs = tokenizer(
-        text, add_special_tokens=False, return_tensors="pt"
-    )
-    tokenized_text = tokenizer.tokenize(text)
-
-    with torch.no_grad():
-        logits = model(**inputs).logits
-
-    predicted_token_class_ids = logits.argmax(-1)
-
-    # Note that tokens are classified rather then input words which means that
-    # there might be more predicted token classes than words.
-    # Multiple token classes might account for the same word
-    predicted_tokens_classes = [model.config.id2label[t.item()] for t in predicted_token_class_ids[0]]
-    predicted_tokens_classes
+    obj = RobertaNER()
+    print(obj.get_entity('I am om rastogi'))
